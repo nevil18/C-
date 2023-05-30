86e95d95fe16a3854fe0d0c54aafa7bf6eb8dee9
nevil18 <nevilanghan2005@gmail.com>
2023-05-30 18:47:28 +0530

    CH-6.1

---
+++ CH-6.1/NEVIL10.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int i=1;
+	clrscr();
+		while(i<=10)
+		{
+			if(i%2== 1);
+			{
+			printf("%d\n",i);
+			}
+			i++;
+		}
+
+ getch();
+}
---
+++ CH-6.1/NEVIL12.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int i=1,n;
+	clrscr();
+	printf("enter n: ");
+	scanf("%d",&n);
+		while(i<=n)
+		{
+			printf("%d\n",i);
+			i++;
+		}
+
+ getch();
+}
