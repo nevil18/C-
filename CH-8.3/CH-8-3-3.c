63a15d741c0f639dc535eef9e4014e59c727d78a
nevil18 <nevilanghan2005@gmail.com>
2023-06-15 19:00:10 +0530

    CH-8.3

---
+++ CH-8.3/CH-8-3-3.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+main()
+
+{
+	int r,c;
+	
+	printf("enter row: ");
+	scanf("%d",&r);
+	printf("enter column: ");
+	scanf("%d",&c);
+	
+	int a[r][c],i,j,sum=0;
+	
+	for(i=0;i<r;i++)
+	{
+		for(j=0;j<c;j++)
+		{
+			printf("enter a[%d][%d]: ",i,j);
+			scanf("%d",&a[i][j]);	
+		}
+	}
+	
+
+		for(i=0;i<r;i++)
+	{
+		for(j=0;j<c;j++)
+		{
+			if(i=j)
+			{
+				sum+=a[i][j];
+			}
+	    }
+	}
+	
+	printf("sum of diagonal elements: %d",sum);
+	
+}
